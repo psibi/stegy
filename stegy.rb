58cbceb6a3673126c2b934c641424efae8c4b029
Sibi <sibi@psibi.in>
2012-04-20 21:35:12 +0530

    Added slot changeModeEntries()

--- stegy.rb
+++ stegy.rb
@@ -2,11 +2,13 @@ require 'stegy_ui'
 
 class Stegyapp < Qt::MainWindow
 
-  slots 'getCoverFile()','getEmbedFile()'
+  slots 'getCoverFile()','getEmbedFile()','changeModeEntries()'
 
   def initialize(parent = nil)
     super(parent)
     algo_name = ['rijndael-128','cast-128', 'gost','twofish','arcfour','cast-256','loki97','rijndael-192','saferplus','wake','des','rijndael-256','serpent','xtea','blowfish','enigma','rc2','tripledes']  
+    @algo_modes = ['cbc','cfb','ctr','ecb','ncfb','nofb','ofb']
+    @stream_modes = ['stream'] #For arcfour, wake and enigma
     @ui = Ui_Stegy.new
     @ui.setup_ui(self)
     algo_name.each do |aname|
@@ -21,4 +23,18 @@ class Stegyapp < Qt::MainWindow
   def getEmbedFile()
     @EmbedFile = Qt::FileDialog.getOpenFileName(self,tr("Get Embed File"),".", tr("Embed Files (*.*)"))
   end
+
+  def changeModeEntries()
+    @ui.mode_comboBox.clear
+    if @ui.algo_comboBox.currentText == "arcfour" or @ui.algo_comboBox.currentText == "wake" or @ui.algo_comboBox.currentText == "enigma"
+      @stream_modes.each do |entries|
+        @ui.mode_comboBox.addItem entries
+      end
+    else
+      @algo_modes.each do |entries|
+        @ui.mode_comboBox.addItem entries
+      end
+    end
+  end
+
 end

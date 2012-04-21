c6574e0baee771fe68c5e5575d526612e38ca018
Sibi <sibi@psibi.in>
2012-04-22 01:47:20 +0530

    License headers added

--- main.rb
+++ main.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2012 Sibi <sibi@psibi.in>
+#
+# This file is part of stegy.
+#
+# Stegy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Stegy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Stegy.  If not, see <http://www.gnu.org/licenses/>.
 require 'Qt4'
 require 'stegy'
  
